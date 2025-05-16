5f87590871b998ee5b3ffe58b11e6787e67cd160
Hamza Safi <safihamza395@gmail.com>
2025-05-16 10:15:19 +0300

    Lecture files are added

---
+++ lecture/src/App.jsx
@@ -0,0 +1,106 @@
+const lectureData = [
+  {
+    title:"Usul",
+    speaker:"Ustaz Muhammad",
+    length:120,
+    category:"Tawheed",
+    poster:"/quran-holy-book-of-muslims.webp",
+    isAvailable:true
+
+  },
+  {
+    title:"Usul",
+    speaker:"Ustaz Muhammad",
+    length:120,
+    category:"Tawheed",
+    poster:"/quran-holy-book-of-muslims.webp",
+    isAvailable:false
+
+  },
+  {
+    title:"Usul",
+    speaker:"Ustaz Muhammad",
+    length:120,
+    category:"Tawheed",
+    poster:"/quran-holy-book-of-muslims.webp",
+    isAvailable:false
+
+  },
+  {
+    title:"Usul",
+    speaker:"Ustaz Muhammad",
+    length:120,
+    category:"Tawheed",
+    poster:"/quran-holy-book-of-muslims.webp",
+    isAvailable:true
+
+  },
+  {
+    title:"Usul",
+    speaker:"Sheikh Muhammad",
+    length:120,
+    category:"Tawheed",
+    poster:"/quran-holy-book-of-muslims.webp",
+    isAvailable:true
+  },
+]
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <LectureList/>
+     <AvailableLectures  />
+    </>
+  )
+}
+
+function Header() {
+  return <header className="header">
+            <h1>Islamic lecture catalog App</h1>
+         </header>
+}
+
+function LectureList() {
+  if (lectureData.length < 1) return <span className="updatedSoon">Updated Soon</span>
+  if(lectureData.length >=1) return <ul className="container">
+          {
+            lectureData.map((lecture)=><Lecture lecture={lecture} key={lecture.title}></Lecture>)
+          }
+        </ul>
+}
+
+function Lecture({lecture}) {
+  
+  return     <li className="lecture">
+                <img src={lecture.poster}></img>
+                <h2>{lecture.title}</h2>
+                <h3>{lecture.speaker}</h3>
+                <p>{lecture.category}</p>
+                <p>{lecture.isAvailable ? lecture.length+" min" : "Coming Soon"}</p>
+                <p>{lecture.isAvailable ? "Available Now" : "Not Available"}</p>
+             </li>
+}
+
+function AvailableLectures() {
+        return <ul className="availabel-aontainer">
+        <h3 className="availabeheader">AvailableLectures List</h3>
+        {
+          lectureData.map((lecture)=> {
+            if(lecture.isAvailable) return <AvailableLecture Available={lecture}></AvailableLecture>
+          })
+        }
+              </ul>
+}
+function AvailableLecture({Available}) {
+  return  <li className="Available">
+                <img src={Available.poster}></img>
+                <h2>{Available.title}</h2>
+                <h3>{Available.speaker}</h3>
+                <p>{Available.category}</p>
+                <p>{Available.length} min</p>
+                <p>{Available.isAvailable}</p>
+             </li>
+}
+export default App
